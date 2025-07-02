e6314e392751fc75bd95225ff0260280f4c2904b
dzulia-2005 <159149786+dzulia-2005@users.noreply.github.com>
2025-07-03 02:21:31 +0400

    create CalculateInvestmentResult function and use it

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,4 +12,32 @@ import {UserInputComponent} from './user-input/user-input.component';
   ],
   standalone: true
 })
-export class AppComponent {}
+export class AppComponent {
+  CalculateInvestmentResult( data : {
+    initialInvestment:number;
+    duration:number;
+    expectedValue:number;
+    annualInvestment:number;
+  }){
+    const {initialInvestment,duration,expectedValue,annualInvestment} = data
+    const annualData = [];
+    let investmentValue = initialInvestment;
+    for(let i=0; i<duration; i++){
+      const year = i+1;
+      const InterestEarnedYear = investmentValue*(expectedValue / 100);
+
+      investmentValue += InterestEarnedYear + annualInvestment;
+      const totalInterest = investmentValue - annualInvestment * year - investmentValue;
+      annualData.push({
+        year: year,
+        Interest: InterestEarnedYear,
+        valueEndOfYear : investmentValue,
+        annualInvestment: investmentValue,
+        totalInterest: totalInterest,
+        totalAmountInvested:initialInvestment + annualInvestment * year
+      })
+    }
+    console.log(annualData);
+  }
+
+}
--- src/app/user-input/user-input.component.ts
+++ src/app/user-input/user-input.component.ts
@@ -1,9 +1,32 @@
-import { Component } from '@angular/core';
+import {Component, EventEmitter, Output} from '@angular/core';
+import {FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-user-input',
   templateUrl: './user-input.component.html',
   styleUrl: './user-input.component.css',
   standalone:true,
+  imports:[FormsModule]
 })
-export class UserInputComponent {}
+export class UserInputComponent {
+  @Output() calculate = new EventEmitter<{
+    initialInvestment:number;
+    duration:number;
+    expectedValue:number;
+    annualInvestment:number;
+  }>();
+  enterInitialInvestment = '0';
+  enterAnnualInvestment = '0';
+  enterExpectedReturn = '5';
+  enterDuration = '10';
+
+  onSubmit() {
+    console.log("SUBMITED");
+    this.calculate.emit({
+      initialInvestment : +this.enterInitialInvestment,
+      duration: +this.enterDuration,
+      expectedValue: +this.enterExpectedReturn,
+      annualInvestment: +this.enterAnnualInvestment
+    });
+  }
+}
